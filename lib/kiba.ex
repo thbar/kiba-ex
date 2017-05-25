4a9d2b8bbc51b73c9617b89c40c4b3d45d0e4180
Thibaut Barrère <thibaut.barrere@gmail.com>
2017-05-25 14:13:22 +0200

    Add boilerplate mix project

---
+++ lib/kiba.ex
@@ -0,0 +1,18 @@
+defmodule Kiba do
+  @moduledoc """
+  Documentation for Kiba.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Kiba.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
